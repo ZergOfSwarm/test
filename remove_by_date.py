633f2f6ded51180ae2a95347201893127a0d0f93
ZergOfSwarm <d358468115905@gmail.com>
2019-05-30 19:36:59 +0300

    удаляет по дате supergit add .!

--- remove_by_date.py
+++ remove_by_date.py
@@ -17,24 +17,22 @@ def main():
 
     for i in full_list:
         selected_list.append(i[23:27] + i[28:30] + i[31:33] + i[35:37] + i[38:40] + i[41:43])
-        print ('Выделили только дату и время из имен файлов!')
-        print(selected_list,end='\n\n')
-        print ('Выбранные элименты сортируем по дате!')
-        selected_list.sort(key=lambda date: datetime.strptime(date,"%Y%m%d%H%M%S"))
-        print(selected_list,end='\n\n')
+    print ('Выделили только дату и время из имен файлов!')
+    print(selected_list,end='\n\n')
+    print ('Выбранные элименты сортируем по дате!')
+    selected_list.sort(key=lambda date: datetime.strptime(date,"%Y%m%d%H%M%S"))
+    print(selected_list,end='\n\n')
 
 
-        files_to_live = selected_list[-N:] # Делаем срез, что бы выделить нужные бакапы!
-        print ('Список файлов  которые будут оствлены -{}'.format(files_to_live),end='\n\n')
-        files_to_delete = selected_list[:-N] # Делаем срез, что бы вылелить список удаляемых файлов!
-        print()
-        print('Список файлов который удаляем!-{}'.format(files_to_delete),end='\n\n')
+    files_to_live = selected_list[-N:] # Делаем срез, что бы выделить нужные бакапы!
+    print ('Список файлов  которые будут оствлены -{}'.format(files_to_live),end='\n\n')
+    files_to_delete = selected_list[:-N] # Делаем срез, что бы вылелить список удаляемых файлов!
+    print()
+    print('Список файлов который удаляем!-{}'.format(files_to_delete),end='\n\n')
 
-        for i in files_to_delete: # Перебираем весь список.
-            #os.remove(i) # Удалем!
-            print('Удалили -{}'.format(i)) # Выписываем все элименты которые бедут удалены.
-
-
+    for i in files_to_delete: # Перебираем весь список.
+        #os.remove(i) # Удалем!
+        print('Удалили -{}'.format(i)) # Выписываем все элименты которые бедут удалены.
 
 
 if (__name__ == "__main__"):
